4cad2f7becbc28ffb6551d92f89fbeaa9d59b01d
shakeen <shakeenkumar33@gmail.com>
2024-11-04 18:48:43 +0530

    sv

--- Templates.js
+++ Templates.js
@@ -1,17 +1,14 @@
-let participantCount = 1;
-
-// Function to create a new participant input set
-function createParticipantElement() {
-    participantCount++;
-    const participantDiv = document.createElement('div');
-    participantDiv.classList.add('participant');
-
-    participantDiv.innerHTML = `
-        <label for="participantName${participantCount}">Participant Name:</label>
-        <input type="text" class="participant-name" id="participantName${participantCount}" name="participantName" required>
-
-        <label for="participantFee${participantCount}">Fee ($):</label>
-        <input type="number" class="participant-fee" id="participantFee${participantCount}" name="participantFee" required>
+// Function to create participant template
+function participantTemplate(count) {
+    return `
+        <div class="participant${count}">
+            <label for="fee${count}">Participant Fee:</label>
+            <input type="number" id="fee${count}" class="fee" required>
+        </div>
     `;
-    return participantDiv;
+}
+
+// Function to create success message template
+function successTemplate(info) {
+    return `Thank you ${info.name} for registering. You have registered ${info.participants} participants and owe $${info.totalFees} in fees.`;
 }
